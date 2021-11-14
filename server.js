7fb25f450dddce426be2a4f2800e9871fab70e86
emmanz95 <emmanza2@gmail.com>
2021-11-14 19:39:35 +0000

    new changes

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const { URIKEY } = require('./uriVariable');
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
-app.use(express.static('public'));
+app.use(express.static("public"))
+app.use(express.static(__dirname + "public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,18 +28,39 @@ app.post('/', (req, res) => {
         if(error && docs.statusCode !== 200) {
             throw error;
         }
-        let forcast = "For city "+city+', country '+country;
 
         const weatherData = {
             weather: body.main,
             description: body.description,
-            icon: body.icon,
+            icon: body.weather.icon,
             main: body.main.temp,
             country: (body.sys.country) ? body.sys.country : '',
         }
+        console.log(weatherData.icon)
+        let forecast = "For city "+city+', country '+ weatherData.country;
 
-        res.render('index', { body: body, forecast: forcast, weatherData: weatherData });
+        res.render('index', { body: body, forecast: forecast, weatherData: weatherData });
     })
+    // axios.post(apiUrl).then((response) => {
+    //     const body = response.data;
+    //     console.log(body);
+    //
+    //     if(response.status !== 200) {
+    //         throw error;
+    //     }
+    //
+    //     const weatherData = {
+    //         weather: body.weather.main,
+    //         description: body.description,
+    //         icon: body.icon,
+    //         main: body.main.temp,
+    //         country: (body.sys.country) ? body.sys.country : '',
+    //     }
+    //     // console.log(weatherData.weather)
+    //     let forecast = "For city "+city+', country '+ weatherData.country;
+    //
+    //     res.render('index', { body: body, forecast: forecast, weatherData: weatherData });
+    // })
 })
 
 app.listen(PORT, () => {
